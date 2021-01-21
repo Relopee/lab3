2d4a3788185e99f50110343c54300fc4e7a5f2d5
Jazmin Lopez <jalopez@ucsd.edu>
2021-01-21 11:45:35 -0800

    added Javascript!

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,26 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("You are connected!!");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").addClass("active");
+		$(".jumbotron p").toggleClass("active");
 	});
-
+	$("a.thumbnail").click(projectClick);
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e){
+	console.log("Project clicked");
+	e.preventDefault();
+	//$(this).css("background-color", "#7fff00");
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+	if(description.length== 0){
+		containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+	}
+	else{
+    	$(description).fadeOut();
+	}
 }
